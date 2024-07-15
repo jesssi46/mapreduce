39aa77212a449f6e9dbb80917cd0b7913ed01b30
jesssi46 <147501700+jesssi46@users.noreply.github.com>
2024-07-15 15:42:50 +0200

    Exercise 1

--- mapper.py
+++ mapper.py
@@ -5,7 +5,7 @@ import sys
 for line in sys.stdin:
     data = line.strip().split("\t")
 
-    if len(data) == 6:
-        date, time, item, category, sales, payment = data
-        sys.stdout.write("{0}\t{1}\n".format(category, sales))
+    
+    date, time, item, category, sales, payment = data
+    sys.stdout.write("{0}\t{1}\n".format(category, sales))
         
