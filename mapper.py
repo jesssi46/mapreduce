9a9e6351aa800aa9c8fbf90dee97c99ec6e88945
jesssi46 <147501700+jesssi46@users.noreply.github.com>
2024-07-15 16:05:35 +0200

    Exercise 4

--- mapper.py
+++ mapper.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python
-#Exercise 3: Adding error message when the number of elements is not 6
+#Exercise 4: Creating a subset of categorynthat should contain: "Computers", "Cameras", "Video Games"
 
 import sys
 
 for line in sys.stdin:
-
     data = line.strip().split("\t")
     if len(data) != 6:
         raise ValueError("Expected 6 elements")
     date, time, item, category, sales, payment = data
+    if category in ["Computers", "Cameras", "Video Games"]:
 #changed from payment to category
-    sys.stdout.write("{0}\t{1}\n".format(category, sales))
+        sys.stdout.write("{0}\t{1}\n".format(category, sales))
