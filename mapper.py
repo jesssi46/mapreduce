8d45dd2e1f65a10e54ab3742ba71a558c63f46b7
jesssi46 <147501700+jesssi46@users.noreply.github.com>
2024-07-15 16:03:20 +0200

    Exercise 3

--- mapper.py
+++ mapper.py
@@ -1,11 +1,13 @@
 #!/usr/bin/env python
+#Exercise 3: Adding error message when the number of elements is not 6
 
 import sys
 
 for line in sys.stdin:
-    data = line.strip().split("\t")
 
-    
+    data = line.strip().split("\t")
+    if len(data) != 6:
+        raise ValueError("Expected 6 elements")
     date, time, item, category, sales, payment = data
+#changed from payment to category
     sys.stdout.write("{0}\t{1}\n".format(category, sales))
-        
